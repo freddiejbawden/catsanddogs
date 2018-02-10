1cff9a2576ca96ace8237c07bef2605d2c331f87
hhowley <33693278+hhowley@users.noreply.github.com>
2018-02-10 13:17:17 +0000

    its raining animals some1 help plz

---
+++ Scripts/Animal.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour {
+	public bool isDog;
+	private float fallspeed;
+	// Use this for initialization
+	void Start () {
+		if (isDog) {
+			initDog();
+		} else {
+			initCat();
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.forward * Manager.instance.timeModifier * 180);
+
+		Vector2 pos = new Vector2(transform.position.x, transform.position.y - fallspeed * Manager.instance.timeModifier);
+		transform.position = pos;
+	}
+
+	void initDog () {
+		fallspeed = 30.0f;
+	}
+
+	void initCat () {
+		fallspeed = 150.0f;
+	}
+
+	void OnCollisionEnter2D(Collision2D col) {
+		if (col.gameObject.tag.Contains("Ground")) {
+			Destroy(gameObject);
+		}
+
+		Debug.Log("Test");
+
+	}
+
+}
---
+++ Scripts/ObjectSpawner.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectSpawner : MonoBehaviour {
+	public float spawnTime;
+	float time = 0;
+	public GameObject[] animals = new GameObject[2];
+
+	// Use this for initialization
+	void Start () {
+		spawnTime = spawnTimer();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		time += Manager.instance.timeModifier;
+		spawner();
+	}
+
+	void spawner () {
+		if (time >= spawnTime) {
+			if (Random.Range(0,2) % 2 == 0) {
+				spawnEntity(animals[0]);
+			} else {
+				spawnEntity(animals[1]);
+			}
+			time = 0;
+			spawnTime = spawnTimer() / 5.0f;
+		}
+	}
+
+	void spawnEntity (GameObject animal) {
+		Vector2 pos = new Vector2(transform.position.x + spawnPos(), transform.position.y);
+		Instantiate(animal, pos, Quaternion.identity);
+	}
+
+	float spawnPos() {
+		float randomFloat = Random.Range(-50,50);
+		return randomFloat;
+	}
+
+	float spawnTimer() {
+		int randomInt = Random.Range(1,10);
+		return (float)randomInt;
+	}
+
+}
