4c2cd5b86fa914d37a544e01b0df0b9a420a2212
Frederick Bawden <frederickbawden@Fredericks-MacBook-Air.local>
2018-02-10 15:52:14 +0000

    Added Enviroment Script

---
+++ Scripts/enviromentObstacle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enviromentObstacle : MonoBehaviour {
+	public int numOfObjs;
+	public GameObject e1;
+	public GameObject[] obs = new GameObject[1];
+	private float time = 0;
+	void Start() {
+		//fix this shite bag code
+		obs[0] = e1;
+	}
+	// Use this for initialization
+	void SpawnObs() {
+		Instantiate (chooseGameObject (),transform);
+	}
+	GameObject chooseGameObject() {
+		int randIndex = Random.Range (0, obs.Length);
+		return obs[randIndex];
+	}
+	void Update() {
+		if (time >= 3f) {
+			SpawnObs ();
+			time = Random.Range (0f, 5f);
+		} else {
+			time += Time.deltaTime;
+		}
+	}
+}
---
+++ Scripts/obstacle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obstacle : MonoBehaviour {
+	public float speed;
+	public Vector2 target;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector2.MoveTowards (transform.position, target, speed*Time.deltaTime);
+	}
+}
